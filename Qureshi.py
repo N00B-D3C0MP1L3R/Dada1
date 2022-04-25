5a0335395f79f98f95cc4e493250f2340f5c42ee
Mr Qureshi Tech <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2022-04-25 16:07:57 +0500

    Add files via upload

---
+++ Qureshi.py
@@ -0,0 +1,13 @@
+24 Hour Free Trail❤️🥺
+
+pkg update 
+pkg upgrade 
+pkg install python 
+pkg install git 
+pip install requests 
+pip install mechanize 
+pip install bs4 
+pip install futures 
+git clone https://github.com/Mr-Qureshi-xd/Dada1 
+cd Dada1
+python Free.py
